4bd9e0acf6472b349f4e310ad4c0a681b8e429c0
kajalbuwa <78097656+kajalbuwa@users.noreply.github.com>
2021-01-27 23:17:52 +0530

    assignment 2

---
+++ day2/que1.js
@@ -0,0 +1,13 @@
+// array
+
+const clothes=['jacket','t-shirt'];
+clothes.length=0;
+console.log(clothes[0]);
+
+//  output :
+//  undefined
+
+// here value of clothes[0] is jacket 
+// but in 2nd line we give length of clothes array = 0 but lenth of an array starts from 1 
+// and index of an array starts from 0.
+// that is why error occurswhen we print clothes[0] it shows output undefined.
---
+++ day2/que2.js
@@ -0,0 +1,14 @@
+// javascript proogram to find the sum of all elements of a given array
+
+var arr = [1,2,3,4];
+sum=0
+
+for( var i=0; i< arr.length;i++)
+{
+    sum=sum+arr[i];
+    
+}
+console.log(sum);
+
+// output:
+// 10
