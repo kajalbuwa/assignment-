176d0e15ab4330656873430a21d3e4938e7ad44c
kajalbuwa <78097656+kajalbuwa@users.noreply.github.com>
2021-01-28 00:59:13 +0530

    assignment4 q2

---
+++ day5 q2/que2.js
@@ -0,0 +1,8 @@
+function add()
+{
+    var num1=parseInt(document.getElementById("first").value);
+    var num2=parseInt(document.getElementById("second").value);
+
+
+    var num2=document.getElementById("result").value=num1+num2;
+}
